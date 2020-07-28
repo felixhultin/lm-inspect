60d5fc0983682fa3fb4c582e9987c55891d27c8c
Felix Hultin <felixhultin@yahoo.se>
2020-07-28 12:19:28 +0200

    another draft

--- lm_inspect.py
+++ lm_inspect.py
@@ -81,17 +81,30 @@ class Inspector:
             attentions = attentions[:, :, :, token_pos]
         return attentions
 
-    def top_words(self, attentions):
-        pass
+    def agg(self, top, display: str = "words", decode: bool = True, k: int = 1000):
+        if display == "words":
+            topk = top.sum(dim=0).topk(k)
+        elif display == "positions":
+            topk = top.sum(dim=1).topk(k)
+        elif display == "words+positions":
+            topk = top.topk(k)
+        else:
+            msg = ("display value " + display + " "
+                   "is not a valid display value"
+                   "Valid display values are: words, positions, words+positions")
+            raise ValueError(msg)
+        return self.tokenizer.decode(topk.indices)
 
     def agg_by_label(self, label, **kwargs):
         indices = [idx for idx, y in enumerate(self.Y) if y == label]
         attentions = self.scope(self.attentions[indices], **kwargs)
-        top = torch.empty(self.tokenized_inputs.shape[1], self.tokenizer.vocab_size)
-        for idx, zipped in enumerate(zip(self.tokenized_inputs, attentions)):
-            input_ids, att = zipped
-            top += attentions[idx]
-        return top
+        top = torch.zeros(self.tokenized_inputs.shape[1], self.tokenizer.vocab_size)
+        for idx, att in zip(indices, attentions):
+            input_ids = self.tokenized_inputs[idx]
+            n_layers, n_heads, max_size, _ = att.shape
+            att_sum = att.sum(dim=0).sum(dim=0).sum(dim=0) / n_layers / n_heads / max_size
+            top[list(range(max_size)), input_ids] += att_sum
+        return self.agg(top, **kwargs)
 
     def agg_by_word(self, word, layer: int = None, head : int = None):
         indices = [idx for idx, y in self.Y if y == label]
